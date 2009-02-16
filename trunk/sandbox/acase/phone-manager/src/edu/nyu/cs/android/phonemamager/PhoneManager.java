d7f91f6110869478ed9806800590794fc5c5c2ed
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-02-16 18:07:29 +0000

    Initial import.

---
+++ trunk/sandbox/acase/phone-manager/src/edu/nyu/cs/android/phonemamager/PhoneManager.java
@@ -0,0 +1,13 @@
+package edu.nyu.cs.android.phonemamager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PhoneManager extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
