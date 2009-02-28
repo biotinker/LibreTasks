a781541361a020b025020690e9821bdfd6bc8a26
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-02-28 20:51:34 +0000

    Added test application UI

--- trunk/sandbox/acase/phone-manager/src/edu/nyu/cs/android/phonemamager/PhoneManager.java
+++ trunk/sandbox/acase/phone-manager/src/edu/nyu/cs/android/phonemamager/PhoneManager.java
@@ -1,13 +1,21 @@
 package edu.nyu.cs.android.phonemamager;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class PhoneManager extends Activity {
+public class PhoneManager extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+        setListAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, APPLICATIONS));
+        getListView().setTextFilterEnabled(true);
     }
+
+    static final String[] APPLICATIONS = new String[] {
+        "Email", "Messaging", "Dialer", "Weather" };
+
 }
