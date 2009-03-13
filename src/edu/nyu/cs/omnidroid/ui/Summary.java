668207158c7e306e13b4d81dc7188df709b8a943
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-03-13 04:36:12 +0000

    Adding some more UI code.

---
+++ src/edu/nyu/cs/omnidroid/ui/Summary.java
@@ -0,0 +1,14 @@
+package edu.nyu.cs.omnidroid.ui;
+
+import edu.nyu.cs.omnidroid.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Summary extends Activity {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.summary);
+  }
+}
