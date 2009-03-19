8bd00cdba3f18db8993fbe350d751f15a7e36a9d
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-03-19 17:50:40 +0000

    Manually adding trunk code from Pradeep

---
+++ trunk/src/edu/nyu/cs/omnidroid/bkgservice/BRService.java
@@ -0,0 +1,71 @@
+package edu.nyu.cs.omnidroid.bkgservice;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BRService extends Service{
+    /* (non-Javadoc)
+	 * @see android.app.Service#onBind(android.content.Intent)
+	 */
+	IntentFilter Ifilter=new IntentFilter();
+    BroadcastReceiver BR=new BCReceiver();
+    @Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate() {
+    	try
+    	{
+    	// To be deleted from this Code
+    	final String TESTSTRING = new String("SMS,SMS_RECEIVED,Evan,SMS,SMS_SEND,URI"); 
+        FileOutputStream fOut = openFileOutput("UserConfig.txt",MODE_WORLD_READABLE); 
+        OutputStreamWriter osw = new OutputStreamWriter(fOut);  
+        osw.write(TESTSTRING); 
+        osw.flush(); 
+        osw.close();
+        
+        FileInputStream FIn = openFileInput("UserConfig.txt"); 
+        BufferedInputStream bis = new BufferedInputStream(FIn); 
+        DataInputStream dis = new DataInputStream(bis);
+        String line;
+        
+        while((line=dis.readLine())!=null)
+        {                
+        	String[] parts=line.split(",");
+        	Log.i("error",parts[1].toString());
+        	Ifilter.addAction(parts[1].toString());
+        }
+     	registerReceiver(BR, Ifilter);
+     	Logger.write("BroadcastReceiver Registered");
+     	
+	}catch(Exception e)
+	{
+		Log.i("Error",e.getLocalizedMessage());
+		Logger.write("Unable to start BroadcastReceiver");
+	}
+	}
+
+	/* (non-Javadoc)
+	 * @see android.app.Service#onDestroy()
+	 */
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		//unregisterReceiver(BR);
+		super.onDestroy();
+	}
+}
---
+++ trunk/src/edu/nyu/cs/omnidroid/bkgservice/Logger.java
@@ -0,0 +1,33 @@
+package edu.nyu.cs.omnidroid.bkgservice;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public class Logger{
+
+static void write(String str)
+{
+	try
+	{
+	FileOutputStream fOut = new FileOutputStream("/Log.txt"); 
+    OutputStreamWriter osw = new OutputStreamWriter(fOut);  
+    osw.write(str); 
+    osw.flush(); 
+    osw.close();
+	}catch(Exception e){}
+} 
+static String read()
+{
+try
+{
+FileInputStream FIn = new FileInputStream("/Log.txt"); 
+BufferedInputStream bis = new BufferedInputStream(FIn); 
+DataInputStream dis = new DataInputStream(bis);
+String str =dis.readLine();
+return str;
+}catch(Exception e){return e.getMessage();}
+}
+}
