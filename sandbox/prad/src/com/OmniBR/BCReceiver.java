ee4958bdf2a0c88b186afa068b926a972ab3776c
phv204 <phv204@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-03-19 17:12:35 +0000

    Initial Import

---
+++ sandbox/prad/src/com/OmniBR/BCReceiver.java
@@ -0,0 +1,22 @@
+package com.OmniBR;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BCReceiver extends BroadcastReceiver{
+	@Override
+	public void onReceive(Context context, Intent intent)
+	    {
+	    Toast.makeText(context,intent.getAction(),5).show();
+	    Toast.makeText(context, Logger.read(), 5).show();
+	    try{
+		Log.i("Received Intent", intent.getAction());
+		}catch(Exception e)
+		{
+			Log.i("Exception in Intent",e.getLocalizedMessage());
+		}
+		}
+	    }
+	
