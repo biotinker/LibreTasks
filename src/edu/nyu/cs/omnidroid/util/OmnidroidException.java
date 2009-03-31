b85259a0444e9283511369508dfa55071612fbb9
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-03-31 03:10:20 +0000

    Adding generic exception handling classes.

---
+++ src/edu/nyu/cs/omnidroid/util/OmnidroidException.java
@@ -0,0 +1,53 @@
+package edu.nyu.cs.omnidroid.util;
+
+/**
+ * Base exception for Omnidroid specific exceptions.
+ */
+public class OmnidroidException extends Exception {
+  
+  /**
+   * Generated SerialID 
+   */
+  private static final long serialVersionUID = -4127262962368059843L;
+  
+  // The code for an exception instance.
+  private int code;
+
+  /**
+   * Create an exception with a code and a message.
+   * 
+   * @param code
+   *          The code for this exception.
+   * @param message
+   *          The message for this exception.
+   */
+  public OmnidroidException(int code, String message) {
+    super(message);
+    this.code = code;
+  }
+
+  /**
+   * Create an exception with a code, a message and a causal <code>Throwable</code>.
+   * 
+   * @param code
+   *          The code for this exception.
+   * @param message
+   *          The message for this exception.
+   * @param cause
+   *          A <code>Throwable</code> that caused this exception.
+   */
+  public OmnidroidException(int code, String message, Throwable cause) {
+    super(message, cause);
+    this.code = code;
+  }
+
+  /**
+   * Get the code this exception is thrown with.
+   * 
+   * @return The code for this exception.
+   */
+  public int getCode() {
+    return code;
+  }
+
+}
