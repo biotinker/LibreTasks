6a006ce603c86d054ab0287dd69598beac936e29
phv204 <phv204@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-09 22:28:56 +0000

    Changes in BRService

--- trunk/src/edu/nyu/cs/omnidroid/bkgservice/BRService.java
+++ trunk/src/edu/nyu/cs/omnidroid/bkgservice/BRService.java
@@ -1,4 +1,5 @@
 package edu.nyu.cs.omnidroid.bkgservice;
+
 import edu.nyu.cs.omnidroid.util.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,97 +13,90 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+public class BRService extends Service {
+  /*
+   * (non-Javadoc)
+   * 
+   * @see android.app.Service#onBind(android.content.Intent)
+   */
+  IntentFilter Ifilter = new IntentFilter();
+  BroadcastReceiver BR = new BCReceiver();
 
-public class BRService extends Service{
-    /* (non-Javadoc)
-	 * @see android.app.Service#onBind(android.content.Intent)
-	 */
-	IntentFilter Ifilter=new IntentFilter();
-    BroadcastReceiver BR=new BCReceiver();
-    
-    @Override
-	public IBinder onBind(Intent arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public IBinder onBind(Intent arg0) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
-	/** Called when the activity is first created. */
-    @Override
-    public void onCreate() 
-    {
-    	try
-    	{
-    	
-    	// To be deleted from this Code
-    	UGParser ug=new UGParser();
-    	ug.delete_all(getApplicationContext());
-    	ug.write(getApplicationContext(), "Enabled", "True");
-    		HashMap<String,String> HM=new HashMap<String,String>();
-    		HM.put("InstanceName","SMS to Email");
-    		HM.put("EventName","SMS_RECEIVED");
-    	    HM.put("EventApp","SMS");
-			HM.put("FilterType","S_PhoneNum");
-			HM.put("FilterData","212-555-1234");
-			HM.put("ActionName","SEND_EMAIL");
-			HM.put("ActionApp","EMAIL");
-			HM.put("ActionData","ContentProvider_URI");
-			HM.put("EnableInstance","True");
-    	ug.writeRecord(getApplicationContext(),HM);
-    	
-    	
-		HM.put("InstanceName","SMS to Email2");
-		HM.put("EventName","SMS_RECEIVED");
-	    HM.put("EventApp","SMS");
-		HM.put("FilterType","S_PhoneNum");
-		HM.put("FilterData","212-555-1234");
-		HM.put("ActionName","SEND_EMAIL");
-		HM.put("ActionApp","EMAIL");
-		HM.put("ActionData","ContentProvider_URI");
-		HM.put("EnableInstance","True");
-		
-	ug.writeRecord(getApplicationContext(),HM);
-        
-    	//Code starts here
-    	ArrayList<HashMap<String,String>> UCRecords=ug.readRecords(getApplicationContext());
-    	Iterator<HashMap<String,String>> i=UCRecords.iterator();
-    	while(i.hasNext())
-    	{
-        HashMap<String,String> HM1=i.next();
-        //Configure the Intent Filter with the Events
-        if(HM1.get("EnableInstance").equalsIgnoreCase("True"))
-    	Ifilter.addAction(HM1.get("EventName"));
-    	}
-    	
-        /*Check the User Config to start OmniDroid*/
-        String Enabled=ug.readLine(getBaseContext(), "Enabled"); 
-        if(Enabled.equalsIgnoreCase("True"))
-        {
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate() {
+    try {
+
+      // To be deleted from this Code
+      UGParser ug = new UGParser(getApplicationContext());
+      ug.update(getApplicationContext(), "Enabled", "True");
+      ug.delete_all();
+      HashMap<String, String> HM = new HashMap<String, String>();
+      HM.put("InstanceName", "SMS to Email");
+      HM.put("EventName", "SMS_RECEIVED");
+      HM.put("EventApp", "SMS");
+      HM.put("FilterType", "S_PhoneNum");
+      HM.put("FilterData", "212-555-1234");
+      HM.put("ActionName", "SEND_EMAIL");
+      HM.put("ActionApp", "EMAIL");
+      HM.put("ActionData", "ContentProvider_URI");
+      HM.put("EnableInstance", "True");
+      ug.writeRecord( HM);
+
+      HM.put("InstanceName", "SMS to Email2");
+      HM.put("EventName", "SMS_RECEIVED");
+      HM.put("EventApp", "SMS");
+      HM.put("FilterType", "S_PhoneNum");
+      HM.put("FilterData", "212-555-1234");
+      HM.put("ActionName", "SEND_EMAIL");
+      HM.put("ActionApp", "EMAIL");
+      HM.put("ActionData", "ContentProvider_URI");
+      HM.put("EnableInstance", "True");
+
+      ug.writeRecord( HM);
+
+      // Code starts here
+      ArrayList<HashMap<String, String>> UCRecords = ug.readRecords();
+      Iterator<HashMap<String, String>> i = UCRecords.iterator();
+      while (i.hasNext()) {
+        HashMap<String, String> HM1 = i.next();
+        // Configure the Intent Filter with the Events
+        if (HM1.get("EnableInstance").equalsIgnoreCase("True"))
+          Ifilter.addAction(HM1.get("EventName"));
+      }
+
+      /* Check the User Config to start OmniDroid */
+      String Enabled = ug.readLine( "Enabled");
+      if (Enabled.equalsIgnoreCase("True")) {
         registerReceiver(BR, Ifilter);
-        }
-        else
-        {
-        Toast.makeText(getBaseContext(),"Stopping OmniDroid",5).show();
+      } else {
+        Toast.makeText(getBaseContext(), "Stopping OmniDroid", 5).show();
         unregisterReceiver(BR);
-        }
-        
-     	  	
-     	
-    	}catch(Exception e)   	   
-    	{
-		Log.i("BRService",e.getLocalizedMessage());
-		Log.i("BRService",e.toString());
-		OmLogger.write(getApplicationContext(),"Not able to Enable/Diable Omnidroid");
-		//Logger.write("Unable to start BroadcastReceiver");
-    	}
-	}
+      }
 
-	/* (non-Javadoc)
-	 * @see android.app.Service#onDestroy()
-	 */
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-		
-	}
+    } catch (Exception e) {
+      Log.i("BRService", e.getLocalizedMessage());
+      Log.i("BRService", e.toString());
+      OmLogger.write(getApplicationContext(), "Not able to Enable/Diable Omnidroid");
+      // Logger.write("Unable to start BroadcastReceiver");
+    }
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see android.app.Service#onDestroy()
+   */
+  @Override
+  public void onDestroy() {
+    // TODO Auto-generated method stub
+    super.onDestroy();
+
+  }
 }
