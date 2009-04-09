0eb3931e8b624280e38a13eb3fdf0723ee55a428
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-09 23:08:32 +0000

    Added proper menu of test activities.

--- trunk/src/edu/nyu/cs/omnidroid/tests/TestApp.java
+++ trunk/src/edu/nyu/cs/omnidroid/tests/TestApp.java
@@ -13,30 +13,48 @@ import android.widget.ListView;
  */
 public class TestApp extends ListActivity {
 
-  static final String[] TESTS = new String[] { "Test User Config",
-    "Test App Config Get", "Test Exception Handler" };
+	static final String[] TESTS = new String[] { "Test User Config",
+			"Test App Config Get", "Test CP", "Test Exception Handler" };
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    Log.i(this.getLocalClassName(), "onCreate");
-    super.onCreate(savedInstanceState);
-    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, TESTS));
-    getListView().setTextFilterEnabled(true);
-  }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(this.getLocalClassName(), "onCreate");
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, TESTS));
+		getListView().setTextFilterEnabled(true);
+	}
 
-  @Override
-  protected void onListItemClick(ListView l, View v, int position, long id) {
-    Log.i(this.getLocalClassName(), "Enter");
-    Log.i(this.getLocalClassName(), "listview: " + l);
-    Log.i(this.getLocalClassName(), "view: " + v);
-    Log.i(this.getLocalClassName(), "position: " + position);
-    Log.i(this.getLocalClassName(), "id: " + id);
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
 
-    
-    //Intent i = new Intent();
-    Intent i = new Intent();
-    i.setClassName("edu.nyu.cs.omnidroid.tests","edu.nyu.cs.omnidroid.tests.TestActivity");
-    startActivity(i);
-  }
+		Log.i(this.getLocalClassName(), "Enter");
+		Log.i(this.getLocalClassName(), "listview: " + l);
+		Log.i(this.getLocalClassName(), "view: " + v);
+		Log.i(this.getLocalClassName(), "position: " + position);
+		Log.i(this.getLocalClassName(), "id: " + id);
 
+		// Intent i = new Intent();
+		Intent i = new Intent();
+
+		switch (position) {
+		case 0:
+			i.setClassName("edu.nyu.cs.omnidroid.tests",
+					"edu.nyu.cs.omnidroid.tests.TestUserConfig");
+			break;
+		case 1:
+			i.setClassName("edu.nyu.cs.omnidroid.tests",
+					"edu.nyu.cs.omnidroid.tests.TestAppConfig");
+			break;
+		case 2:
+			i.setClassName("edu.nyu.cs.omnidroid.tests",
+					"edu.nyu.cs.omnidroid.tests.TestCP");
+			break;
+		case 3:
+			i.setClassName("edu.nyu.cs.omnidroid.tests",
+					"edu.nyu.cs.omnidroid.tests.TestExceptionHandler");
+			break;
+		}
+		startActivity(i);
+	}
 }
