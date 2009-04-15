1c793052572d57f6caf544e7bfd9f05ad04670ab
rs2642 <rs2642@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-15 04:44:16 +0000

    Updated Catcher

--- src/edu/nyu/cs/omnidroid/external/catcherapp/SMSListener.java
+++ src/edu/nyu/cs/omnidroid/external/catcherapp/SMSListener.java
@@ -30,23 +30,28 @@ public class SMSListener extends BroadcastReceiver {
     Toast.makeText(context, intent.getAction(), 5).show();
     if (intent.getAction().equalsIgnoreCase("SMS_SENT")) {
       // Bundle bundle = intent.getExtras();
-      Uri DummyURI = Uri.parse("content://edu.nyu.cs.omnidroid.core.maincp/CP");
+      Uri DummyURI = Uri.parse("content://edu.nyu.cs.omnidroid.core.maincp/CP/2");
       /* FOR SPLITTING THE URI
       String str_uri = DummyURI.toString();
       String[] temp = null;
       temp = str_uri.split("/");
       String num = temp[temp.length - 1];
       */
-      String action_data = displayAction(DummyURI.toString(), "2");
+      String action_data = displayAction(DummyURI.toString());
       Toast.makeText(context, action_data, 5);
       // if (bundle != null) {
       // Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
     }
   }
 
-  public String displayAction(String uri, String record) {
+  public String displayAction(String uri) {
+    
+    String str_uri = uri.toString();
+    String[] temp = null;
+    temp = str_uri.split("/");
+    String num = temp[temp.length - 1];
+    int new_id = Integer.parseInt(num);
     Cursor cur = act.managedQuery(Uri.parse(uri), null, null, null, null);
-    int new_id = Integer.parseInt(record);
     if (cur.moveToPosition(new_id)) {
       Toast.makeText(
           act,
