8f976ddef89d1336f3f1096830754baa6ca0b5cb
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-15 03:36:17 +0000

    Removed import warning.

--- trunk/src/edu/nyu/cs/omnidroid/ui/ActionThrowerData.java
+++ trunk/src/edu/nyu/cs/omnidroid/ui/ActionThrowerData.java
@@ -7,7 +7,6 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
