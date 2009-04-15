b88c117011f3e82a973b3cd96b8783dc7adb32ed
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-15 19:22:03 +0000

    Add external app broadcast receiver

--- src/edu/nyu/cs/omnidroid/tests/TestApp.java
+++ src/edu/nyu/cs/omnidroid/tests/TestApp.java
@@ -9,61 +9,75 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 /**
- * This class will present a list of test applications that we can run via UI selection.
+ * This class will present a list of test applications that we can run via UI
+ * selection.
  * 
  * @author acase
  */
 public class TestApp extends ListActivity {
 
-  static final String[] TESTS = new String[] { "Test User Config", "Test App Config Get",
-      "Test CP", "Test Exception Handler", "Test Service", "TestApp", "PackageLister" };
+	static final String[] TESTS = new String[] { "Test User Config",
+			"Test App Config", "Test CP", "Test Exception Handler",
+			"Test Service", "TestApp", "PackageLister", "External Event App" };
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    Log.i(this.getLocalClassName(), "onCreate");
-    super.onCreate(savedInstanceState);
-    setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, TESTS));
-    getListView().setTextFilterEnabled(true);
-  }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Log.i(this.getLocalClassName(), "onCreate");
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, TESTS));
+		getListView().setTextFilterEnabled(true);
+	}
 
-  @Override
-  protected void onListItemClick(ListView l, View v, int position, long id) {
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
 
-    Log.i(this.getLocalClassName(), "Enter");
-    Log.i(this.getLocalClassName(), "listview: " + l);
-    Log.i(this.getLocalClassName(), "view: " + v);
-    Log.i(this.getLocalClassName(), "position: " + position);
-    Log.i(this.getLocalClassName(), "id: " + id);
+		Log.i(this.getLocalClassName(), "Enter");
+		Log.i(this.getLocalClassName(), "listview: " + l);
+		Log.i(this.getLocalClassName(), "view: " + v);
+		Log.i(this.getLocalClassName(), "position: " + position);
+		Log.i(this.getLocalClassName(), "id: " + id);
 
-    Intent i = new Intent();
+		Intent i = new Intent();
 
-    switch (position) {
-    case 0:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestUserConfig");
-      break;
-    case 1:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestAppConfig");
-      break;
-    case 2:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestCP");
-      break;
-    case 3:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestExceptionHandler");
-      break;
-    case 4:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestService");
-      break;
-    case 5:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.tests.TestApp");
-      break;
-    case 6:
-      i.setClassName(this.getApplicationContext(), "edu.nyu.cs.omnidroid.ui.PackageLister");
-      break;
-    default:
-      Log.i(this.getLocalClassName(), "Invalid Test Selection");
-      // TODO (acase): Catch this as an exception
-      break;
-    }
-    startActivity(i);
-  }
+		switch (position) {
+		case 0:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestUserConfig");
+			break;
+		case 1:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestAppConfig");
+			break;
+		case 2:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestCP");
+			break;
+		case 3:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestExceptionHandler");
+			break;
+		case 4:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestService");
+			break;
+		case 5:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.tests.TestApp");
+			break;
+		case 6:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.ui.PackageLister");
+			break;
+		case 7:
+			i.setClassName(this.getApplicationContext(),
+					"edu.nyu.cs.omnidroid.external.eventapp.ExternalCPActivity");
+			break;
+		default:
+			Log.i(this.getLocalClassName(), "Invalid Test Selection");
+			// TODO (acase): Catch this as an exception
+			break;
+		}
+		startActivity(i);
+	}
 }
