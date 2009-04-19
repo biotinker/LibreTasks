d943e9335c16c580044cef5c7a1b035c2ee21e8c
acase <acase@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-19 06:09:57 +0000

    Adding a CheckBox Listview Widget so we can have fancy Listviews with checkboxes. Adding a default layout. Adding some Test classes.

---
+++ trunk/src/edu/nyu/cs/omnidroid/ui/CheckBoxViewWrapper.java
@@ -0,0 +1,54 @@
+/***
+	Copyright (c) 2008-2009 CommonsWare, LLC
+  Copyright (c) 2009 Andrew Case
+	
+	Licensed under the Apache License, Version 2.0 (the "License"); you may
+	not use this file except in compliance with the License. You may obtain
+	a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+ */
+
+package edu.nyu.cs.omnidroid.ui;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+class CheckBoxViewWrapper {
+  ViewGroup base;
+  View guts = null;
+  CheckBox rate = null;
+
+  CheckBoxViewWrapper(ViewGroup base) {
+    this.base = base;
+  }
+
+  CheckBox getCheckBox() {
+    if (rate == null) {
+      rate = (CheckBox) base.getChildAt(0);
+    }
+
+    return (rate);
+  }
+
+  void setCheckBox(CheckBox rate) {
+    this.rate = rate;
+  }
+
+  View getGuts() {
+    if (guts == null) {
+      guts = base.getChildAt(1);
+    }
+
+    return (guts);
+  }
+
+  void setGuts(View guts) {
+    this.guts = guts;
+  }
+}
