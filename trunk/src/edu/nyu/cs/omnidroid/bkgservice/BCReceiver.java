660d99de1a40a9006883b2970e2e4e63d95d50fc
ssg292 <ssg292@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-04-21 22:08:30 +0000

    sms

--- trunk/src/edu/nyu/cs/omnidroid/bkgservice/BCReceiver.java
+++ trunk/src/edu/nyu/cs/omnidroid/bkgservice/BCReceiver.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
 import edu.nyu.cs.omnidroid.util.OmLogger;
@@ -16,21 +17,39 @@ import edu.nyu.cs.omnidroid.util.UGParser;
 import edu.nyu.cs.omnidroid.core.*;
 
 public class BCReceiver extends BroadcastReceiver {
-	Context context;
+	
+	//Context context;
 	@Override
 	public void onReceive(Context context, Intent intent)
-	    {this.context = context;
+	    {
+		//this.context = context;
 		//Toast.makeText(context,"Starting BR",Toast.LENGTH_LONG).show();
 		try{
+			intent.setClass(context, edu.nyu.cs.omnidroid.core.DummyActivity.class);
+			intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+			CharSequence cs = "SMS_RECEIVED";
+		if (intent.getAction().contains(cs))
+		{
 			
-		Toast.makeText(context,intent.getAction(),5).show();
-	    
-	   	/*ProcessIntent p = new ProcessIntent(intent,context);
-		String filter = p.matchEventName();
-		String[] cols = p.getActionData();*/
-		
-		intent.setClass(context, edu.nyu.cs.omnidroid.core.DummyActivity.class);
-		intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+			Bundle bundle = intent.getExtras();
+		    Toast.makeText(context, intent.getAction() , Toast.LENGTH_SHORT).show();
+		    SmsMessage[] msgs = null;
+		    String str = "";
+		    if (bundle != null) {
+		      Object[] pdus = (Object[]) bundle.get("pdus");
+		      msgs = new SmsMessage[pdus.length];
+		      for (int i = 0; i < msgs.length; i++) {
+		        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+		        str += "SMS from " + msgs[i].getOriginatingAddress();
+		        str += " :";
+		        str += msgs[i].getMessageBody().toString();
+		        str += "\n";
+		      }
+		      Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+
+		    }
+		}
+		else
 		context.startActivity(intent);
 		Log.i("Received Intent", intent.getAction());
 		}catch(Exception e)
