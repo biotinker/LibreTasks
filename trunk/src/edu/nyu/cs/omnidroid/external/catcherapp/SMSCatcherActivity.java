980d7dbd066ff5cff5f5dc8e680f6dd79dec5738
rs2642 <rs2642@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-05-05 01:21:14 +0000

    sms catcher

--- trunk/src/edu/nyu/cs/omnidroid/external/catcherapp/SMSCatcherActivity.java
+++ trunk/src/edu/nyu/cs/omnidroid/external/catcherapp/SMSCatcherActivity.java
@@ -17,6 +17,9 @@ import edu.nyu.cs.omnidroid.core.CP;
 public class SMSCatcherActivity extends Activity {
     /** Called when the activity is first created.*/ 
   private String uri;
+  private String uri2;
+  private String text;
+  public String txtmsg;
   Intent intent;  
   
   @Override
@@ -25,8 +28,10 @@ public class SMSCatcherActivity extends Activity {
         //setContentView(R.layout.main2);
         this.intent=getIntent();
         uri = getURI(intent);
-       
-       displayAction(uri.toString());
+        uri2 = getURI2(intent);
+        text= retrieveURI2(uri2.toString());
+        
+       displayAction(uri.toString(),text);
       // deleteSMS();
      this.finish();
   } 
@@ -39,8 +44,50 @@ public class SMSCatcherActivity extends Activity {
     return uri;
     
   }
+       public String getURI2(Intent intent1)
+       {
+         Bundle b = intent1.getExtras();
+         Object c = b.get("uri2");
+         String uri22=c.toString();
+         return uri22;
+       }
+       public String retrieveURI2(String uri2) 
+       {
+         String[] cols = null;
+         String str_uri = uri2;
+           String[] temp = null;
+           temp = str_uri.split("/");
+           String num = temp[temp.length - 1];
+           String final_uri=str_uri.substring(0, str_uri.length()-num.length()-1);
+           int new_id = Integer.parseInt(num);
+           
+           Cursor cur = managedQuery(Uri.parse(final_uri), null, null, null, null);
+           if (cur.moveToFirst()) {
+
+             
+             do {
+
+               int id = Integer.parseInt(cur.getString(cur.getColumnIndex("_id")));
+
+                 
+                if (new_id==id)
+                {
+                  txtmsg = cur.getString(cur.getColumnIndex(CP.ACTION_DATA));
+                  
+                }
+                           
+                 
+             } while (cur.moveToNext());
+
+             }
+           return txtmsg;
+            
+           }
        
-       public void displayAction(String uri) {
+       
+       
+       public void displayAction(String uri, String txtmsg) 
+       {
     	   String[] cols = null;
     	   String str_uri = uri;
     	     String[] temp = null;
@@ -61,13 +108,14 @@ public class SMSCatcherActivity extends Activity {
     	          if (new_id==id)
     	          {
     	            String number = cur.getString(cur.getColumnIndex(CP.ACTION_DATA));
+    	           // String txtmsg = cur.getString(cur.getColumnIndex(CP.));
     	            Toast.makeText(
     	                getBaseContext(),
     	                /*cur.getString(cur.getColumnIndex(CP._ID)) + ","
     	                +*/ number, Toast.LENGTH_LONG).show();
     	            try
     	            {
-    	            	sendSMS(number,"DND");
+    	            	sendSMS(number,txtmsg);
     	            }catch(Exception e){e.toString();}
     	          }
     	                     
