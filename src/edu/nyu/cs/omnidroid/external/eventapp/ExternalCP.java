3c51f190cdd94b4e0d05e72675b177454e4e952e
rs2642 <rs2642@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-05-12 05:53:06 +0000

    added comments/java-docs

--- src/edu/nyu/cs/omnidroid/external/eventapp/ExternalCP.java
+++ src/edu/nyu/cs/omnidroid/external/eventapp/ExternalCP.java
@@ -13,6 +13,15 @@ import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.util.Log;
 
+/**
+ *  ExternalCP class is used to create a content provider for the 
+ *  external event application.
+ *  
+ * @author      Rajiv Sharma
+ */
+
+
+
 public class ExternalCP extends ContentProvider {
   public static final String CP_Name = "edu.nyu.cs.omnidroid.external.eventapp.cp";
   public static final Uri CONTENT_URI = Uri.parse("content://" + CP_Name + "/CP");
