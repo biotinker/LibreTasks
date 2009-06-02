aae4de11e717d6d75dfde4030044f6bc3cbf1db9
case.andrew <case.andrew@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-06-02 19:58:50 +0000

    Updating autobuild to work with the new google code repo.

--- tools/autobuild/ant-build.sh
+++ tools/autobuild/ant-build.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 #PROJECT_HOME=`dirname "$0"`
-BUILD_DIR="/home/acase/classes/itp/build"
-REPO_URL="https://subversive.cims.nyu.edu/itp/sp09-google"
+BUILD_DIR="/home/acase/projects/omnidroid/build"
+REPO_URL="http://omnidroid.googlecode.com/svn/"
 CO_DIR="$BUILD_DIR/work/checkout"
-TEST_DIR="$BUILD_DIR/work/test"
+TEST_DIR="$CO_DIR/trunk"
 ANDROID_DIR="$BUILD_DIR/tools/android-sdk-linux_x86-1.1_r1"
 ANDROID_BIN_DIR="$ANDROID_DIR/tools"
-PKG_NAME="edu.nyu.cs.omnidroid"
+MANIFEST="AndroidManifest.xml"
 LOG_DIR="$BUILD_DIR/logs"
 export JAVA_HOME="/opt/java"
 export PATH="/usr/local/bin:/opt/bin:/opt/java/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
 cleanup() {
     # Cleanup
-    echo "  + Removing $TEST_DIR" && \
-    rm -rf $TEST_DIR && \
     echo "  + Removing $CO_DIR" && \
     rm -rf $CO_DIR && \
     mkdir -p $CO_DIR && \
@@ -30,16 +28,14 @@ checkout() {
     cd $CO_DIR && \
     svn co $REPO_URL/trunk
     return $?
-    #svn co $REPO_URL/tools/cruisecontrol && \
 }
 
 build() {
     # Build it
     echo "  + Using android tools to build an ant file" && \
-    cd $CO_DIR/trunk && \
-    env && \
-    $ANDROID_BIN_DIR/activitycreator --out $TEST_DIR $PKG_NAME && \
     cd $TEST_DIR && \
+    env && \
+    $ANDROID_BIN_DIR/activitycreator --out $TEST_DIR $MANIFEST && \
     echo "  + Building from ant build file" && \
     ant
     return $?
--- tools/autobuild/check-build.sh
+++ tools/autobuild/check-build.sh
@@ -5,40 +5,56 @@
 # success or failure
 
 DATE=`date +%Y.%m.%d_%T`
-BUILD_DIR="$HOME/classes/itp/build"
+BUILD_DIR="$HOME/projects/omnidroid/build"
 BUILD_SCRIPT="$BUILD_DIR/bin/ant-build.sh"
 BUILD_FLAG="$BUILD_DIR/.NEEDS_REBUILD"
+REPO_URL="http://omnidroid.googlecode.com/svn/trunk"
 TMPFILE="$BUILD_DIR/var/tmp/check-build"
 EMAIL_HEAD="$BUILD_DIR/share/templates/email_header"
 EMAIL_FOOT="$BUILD_DIR/share/templates/email_footer"
 LOG="$BUILD_DIR/logs/omnidroid.build.log.$DATE"
-TO="itp-sp09-google-private@cims.nyu.edu"
+TO="omnidroid-build@googlegroups.com"
 #TO="acase@cims.nyu.edu"
-SUBJECT="[Build] Rev#"
+REV_OLD_FILE="$BUILD_DIR/.REV_OLD"
+REV_NEW_FILE="$BUILD_DIR/.REV_NEW"
+REV_OLD=""
+REV_NEW=""
+SUBJECT="Rev#"
+
+update_version() {
+    mv $REV_NEW_FILE $REV_OLD_FILE
+    svn info $REPO_URL | grep ^Rev | awk '{print $2}' > $REV_NEW_FILE
+}
 
 initialize() {
     if [ -f $TMPFILE ]; then
         # If this script is already running, then don't build
         exit 1;
-    elif [ ! -f $BUILD_FLAG ]; then
-        # If no build flag has been generated, then don't build
+    fi
+    update_version
+    if [ ! `cat ${REV_NEW_FILE}` -gt `cat ${REV_OLD_FILE}` ]; then
+        # If no update has been made, then don't build
         exit 2;
     else
         # Otherwise, it's off to the races!
         /bin/touch ${TMPFILE}
-        REV=`grep -m 1 "^New Revision" ${BUILD_FLAG} | awk '{print $3}'`
+        REV_NEW=`cat ${REV_NEW_FILE}`
+        REV_OLD=`cat ${REV_OLD_FILE}`
+
     fi
+    touch $LOG
 }
 
 # Build our software
 build() {
-    $BUILD_SCRIPT >$LOG 2>&1 
+    echo "Running $BUILD_SCRIPT" >> $LOG
+    $BUILD_SCRIPT >>$LOG 2>&1 
     if [ $? == "0" ]; then
         build_result=0
-        SUBJECT="$SUBJECT $REV SUCCESSFUL"
+        SUBJECT="$SUBJECT $REV_NEW SUCCESSFUL"
     else
         build_result=1
-        SUBJECT="$SUBJECT $REV FAILURE"
+        SUBJECT="$SUBJECT $REV_NEW FAILURE"
     fi
 }
 
@@ -47,8 +63,10 @@ email_results() {
     echo "to: $TO" >> ${TMPFILE}
     echo "subject: $SUBJECT" >> ${TMPFILE}
     cat ${EMAIL_HEAD} >> ${TMPFILE}
+    echo "== OLD REVISION: ${REV_OLD}" >> ${TMPFILE}
+    echo "== NEW REVISION: ${REV_NEW}" >> ${TMPFILE}
     echo "== REVISION COMMIT LOG =============================" >> ${TMPFILE}
-    cat $BUILD_FLAG >> ${TMPFILE}
+    svn log ${REPO_URL} -v -r ${REV_OLD}:${REV_NEW} >> ${TMPFILE}
     echo "== /REVISION COMMIT LOG ============================" >> ${TMPFILE}
     echo "== BUILD LOG =======================================" >> ${TMPFILE}
     cat $LOG >> ${TMPFILE}
