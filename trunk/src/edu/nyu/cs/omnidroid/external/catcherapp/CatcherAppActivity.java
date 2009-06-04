85740574943ae6eddcd0013d002f9c9f226a0f9a
mschidlowsky <mschidlowsky@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-06-04 16:57:59 +0000

    Removed workspace warnings. Cleaned up some variable names. svn:ignored gen and bin directories. Fixes issue #4.

--- trunk/src/edu/nyu/cs/omnidroid/external/catcherapp/CatcherAppActivity.java
+++ trunk/src/edu/nyu/cs/omnidroid/external/catcherapp/CatcherAppActivity.java
@@ -2,17 +2,17 @@ package edu.nyu.cs.omnidroid.external.catcherapp;
 
 /**The Catcher Application is presently in the application itself. However it can be pulled out of the application and treated as a standalone third party application. It needs to have the following structure:
  *
- *-	A Broadcast Receiver that catches the intents broadcasted by OmniDroid. 
- *-	The Broadcast Receiver can then point to an activity that can perform any relevant operations on the intent. 
+ *-	A Broadcast Receiver that catches the intents broadcasted by OmniDroid.
+ *-	The Broadcast Receiver can then point to an activity that can perform any relevant operations on the intent.
  *-	It also needs to have a mechanism that allows it to query the content provider of OmniDroid
- * 
+ *
  */
 /**
- * Activity used to fetch data from the content provider based on the 
+ * Activity used to fetch data from the content provider based on the
  * URI's received.
- * 
+ *
  * @author Rajiv Sharma
- * 
+ *
  */
 import android.app.Activity;
 import android.content.Intent;
@@ -50,12 +50,10 @@ public class CatcherAppActivity extends Activity {
 
   /**
    * Queries the OmniDroid Content Provider to retrieve the Action Data.
-   * 
-   * @author Rajiv Sharma
+   *
    */
 
   public void displayAction(String uri) {
-    String[] cols = null;
     String str_uri = uri;
     String[] temp = null;
     temp = str_uri.split("/");
@@ -87,8 +85,8 @@ public class CatcherAppActivity extends Activity {
      * = temp[temp.length - 1]; String final_uri=str_uri.substring(0,
      * str_uri.length()-num.length()); int new_id = Integer.parseInt(num); Cursor cur =
      * managedQuery(Uri.parse(final_uri), null, null, null, null);
-     * 
-     * 
+     *
+     *
      * if (cur.moveToPosition(new_id)) { Toast.makeText( getBaseContext(),
      * cur.getString(cur.getColumnIndex(CP._ID)) + "," +
      * cur.getString(cur.getColumnIndex(CP.ACTION_DATA)), Toast.LENGTH_LONG).show(); } String action
