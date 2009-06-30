71362a99fe0b09735dc93c5c1ca86469852d6be5
londinop <londinop@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-06-30 14:09:10 +0000

    Added tests for new core classes.

---
+++ branches/model-update/omnidroid-test/src/edu/nyu/cs/omnidroid/core/RuleTest.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright 2009 OmniDroid - http://code.google.com/p/omnidroid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package edu.nyu.cs.omnidroid.core;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import edu.nyu.cs.omnidroid.tests.TestData;
+
+/**
+ * Unit tests for {@link Rule} class.
+ */
+public class RuleTest extends TestCase {
+  Rule rule;
+  Event event;
+
+  public void setUp() {
+    rule = TestData.getRule();
+    event = TestData.getSMSEvent();
+  }
+  
+  public void testCheckFilters_True() {
+    assertTrue(rule.checkFilters(event));
+  }
+  
+  public void testCheckFilters_False() {
+    rule.filterData[0] = "Some other message";
+    assertFalse(rule.checkFilters(event));
+  }
+  
+  public void testGetActions() {
+    ArrayList<Action> actions = new ArrayList<Action>();
+    Action action = new Action();
+    actions.add(action);
+    assertEquals(actions.size(),rule.getActions(event).size());
+  }
+}
