6c824abb3d1dedd1b0b6cb67ff5235c519c7e69f
markww <markww@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-07-06 21:56:07 +0000

    Adding test folder before commit of simple UI additions.

---
+++ omnidroid/src/edu/nyu/cs/omnidroid/ui/simple/UtilUI.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright 2009 OmniDroid - http://code.google.com/p/omnidroid 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *******************************************************************************/
+package edu.nyu.cs.omnidroid.ui.simple;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+
+/**
+ * Spare parts for UI work.
+ */
+public class UtilUI {
+	
+	/**
+	 * Wraps call to displaying a simple message box.
+	 * @param context
+	 * @param title
+	 * @param message
+	 */
+	public static void showAlert(Context context, String title, String message) {
+		new AlertDialog.Builder(context).setTitle(title)
+	    .setIcon(0)
+	    .setMessage(message)
+	    .setCancelable(true)
+	    .setPositiveButton(
+	      "OK",
+          new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialoginterface, int i) {
+              }
+          }
+	    ).show();
+	}
+	
+	/**
+	 * Force-inflates a dialog main linear-layout to take max available
+	 * screen space even though contents might not occupy full screen
+	 * size.
+	 * @param context
+	 * @param ll
+	 */
+	public static void inflateDialog(LinearLayout ll) {
+		WindowManager wm = (WindowManager)ll.getContext().getSystemService(Context.WINDOW_SERVICE); 
+        Display display = wm.getDefaultDisplay(); 
+        ll.setMinimumWidth(display.getWidth() - 30);
+        ll.setMinimumHeight(display.getHeight() - 40);
+	}
+}
