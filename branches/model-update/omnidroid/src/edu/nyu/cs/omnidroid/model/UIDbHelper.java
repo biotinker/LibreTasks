dd557021be758c872f793191f1cbe25f449f4951
markww <markww@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-07-09 14:29:08 +0000

    Added skeleton code for rest of database functions we'll need with UI.

--- branches/model-update/omnidroid/src/edu/nyu/cs/omnidroid/model/UIDbHelper.java
+++ branches/model-update/omnidroid/src/edu/nyu/cs/omnidroid/model/UIDbHelper.java
@@ -32,6 +32,7 @@ import edu.nyu.cs.omnidroid.ui.simple.model.ModelAttribute;
 import edu.nyu.cs.omnidroid.ui.simple.model.ModelEvent;
 import edu.nyu.cs.omnidroid.ui.simple.model.ModelFilter;
 import edu.nyu.cs.omnidroid.ui.simple.model.Rule;
+import edu.nyu.cs.omnidroid.ui.simple.model.RuleNode;
 
 public class UIDbHelper {
   
@@ -103,6 +104,18 @@ public class UIDbHelper {
   
   public ArrayList<ModelApplication> getApplications() {
 	  // TODO: Return all applications that have usable actions.
+	  /*
+	  ArrayList<ModelApplication> applications = new ArrayList<ModelApplication>();
+	  Cursor cursor = registeredApplicationDbAdapter.fetchAll();
+	  for (int i = 0; i < cursor.getCount(); i++) {
+	      cursor.moveToNext();
+	      applications.add(new ModelApplication(
+	          cursor.getString(cursor.getColumnIndex(RegisteredActionDbAdapter.KEY_APPLICATIONNAME)), 
+	          "", R.drawable.icon_event_unknown));
+	  }
+	  cursor.close();
+	  return applications;
+	  */
 	  return null;
   }
   
@@ -110,12 +123,12 @@ public class UIDbHelper {
 	// TODO: Given a ModelApplication, return all ModelAction associated with it.
 	/*
     ArrayList<ModelAction> actions = new ArrayList<ModelAction>();
-    Cursor cursor = registeredActionDbAdapter.fetchAll();
+    Cursor cursor = registeredActionDbAdapter.fetchAll(null, Long.valueOf(application.getDatabaseId()));
     for (int i = 0; i < cursor.getCount(); i++) {
       cursor.moveToNext();
       actions.add(new ModelAction(
           cursor.getString(cursor.getColumnIndex(RegisteredActionDbAdapter.KEY_ACTIONNAME)), 
-          "", R.drawable.icon_action_unknown, application));
+          "", R.drawable.icon_event_unknown, application));
     }
     cursor.close();
     return actions;
@@ -124,13 +137,60 @@ public class UIDbHelper {
   }
   
   public ArrayList<Rule> getRules() {
-	  // TODO: Return a list of all rules saved in the datbase.
+	  // TODO: Return a list of all rules saved in the database.
+	  // If you can give me a flat list of the event, all filters,
+	  // all actions associated with a Rule, I can construct it
+	  // as a tree in-memory here, something like:
+	  //
+	  // for (every rule in database) {
+	  // 
+	  //   ModelEvent             event   = getRootEventForRule(ruleid);
+	  //   ArrayList<ModelFilter> filters = getFiltersForRule(ruleid);
+	  //   ArrayList<ModelAction> actions = getActionsForRule(ruleid);
+	  //
+	  //   // Then I will construct this rule:
+	  //   restoreRule(event, filters, actions);
+	  // }
 	  return null;
   }
   
   public void saveRule(Rule rule) throws Exception {
 	  // TODO: Given a rule, try to save it to the database.
-	  // First check if the rule has a valid ID - if so, delete all preexisting
-	  // records associated with this rule before saving.
+	  // 1) First check if the rule has a valid ID - if so, delete all preexisting
+	  //    records associated with this rule before saving.
+	  // ...
+	  //
+	  // 2) Save the Rule to the Rule table, I'm guessing this would be something
+	  //    like just a rule name, creation date timestamp etc.
+	  //
+	  // 3) Recursively iterate over all nodes, adding each node to the database.
+	  saveRuleNode(rule.getRootNode());  
+  }
+  
+  /**
+   * Recursively write each node of the rule to the database.
+   * We can use instanceof like below, or add a writeToDatabase()
+   * method for each subclass of <code>ModelItem</code> to make
+   * this cleaner.
+   */
+  private void saveRuleNode(RuleNode node) {
+	  if (node.getItem() instanceof ModelEvent) {
+          ModelEvent event = (ModelEvent)node.getItem();	
+          // TODO: Write this event to the database.
+	  }
+	  else if (node.getItem() instanceof ModelFilter) {
+		  ModelFilter filter = (ModelFilter)node.getItem();
+		  // TODO: Write this filter to the database.
+		  // filter.getFilterData() will give you the associated OmniData.
+	  }
+	  else if (node.getItem() instanceof ModelAction) {
+		  ModelAction action = (ModelAction)node.getItem();
+		  // TODO: Write this action to the database.
+	  }
+	  
+	  // Now all children.
+	  for (int i = 0; i < node.getChildren().size(); i++) {
+		  saveRuleNode(node.getChildren().get(i));
+	  }
   }
 }
