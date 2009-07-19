7b3105d13647a83994d7073b2bc4a0e74cdc5048
markww <markww@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-07-19 22:08:16 +0000

    Adding first new UI screen, the main activity. The application will not use this activity until all other new UI activities are also reviewed and added to trunk.

---
+++ omnidroid/src/edu/nyu/cs/omnidroid/ui/simple/ActivityMain.java
@@ -0,0 +1,106 @@
+/*******************************************************************************
+ * Copyright 2009 OmniDroid - http://code.google.com/p/omnidroid 
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ *     
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ *******************************************************************************/
+package edu.nyu.cs.omnidroid.ui.simple;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import edu.nyu.cs.omnidroid.R;
+
+/**
+ * This is the main entry point of the application. Here the user will see a main menu where they
+ * can choose to create a new rule, view existing rules, or see help items.
+ */
+public class ActivityMain extends Activity {
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // Link up click handlers with their buttons.
+    Button btnCreateRule = (Button) findViewById(R.id.activity_main_btnCreateRule);
+    btnCreateRule.setOnClickListener(listenerBtnClickCreateRule);
+
+    Button btnViewRules = (Button) findViewById(R.id.activity_main_btnViewRules);
+    btnViewRules.setOnClickListener(listenerBtnClickViewRules);
+
+    Button btnViewLogs = (Button) findViewById(R.id.activity_main_btnEventLog);
+    btnViewLogs.setOnClickListener(listenerBtnClickViewLogs);
+
+    Button btnHelp = (Button) findViewById(R.id.activity_main_btnHelp);
+    btnHelp.setOnClickListener(listenerBtnClickHelp);
+  }
+
+  /**
+   * Launch the create-a-new-rule activity.
+   */
+  private OnClickListener listenerBtnClickCreateRule = new OnClickListener() {
+    public void onClick(View v) {
+      // Wipe UI state for the the choose root event activity. The choose
+      // root event activity cannot tell if its onDestroy() method is being
+      // called in response to the user going back here, or just due to a
+      // rotation. So we let this parent activity wipe state.
+      
+      // TODO: Reenable once all UI classes are added to trunk:
+      /*
+      SharedPreferences state = v.getContext().getSharedPreferences(
+          ActivityChooseRootEvent.KEY_STATE,
+          Context.MODE_WORLD_READABLE | Context.MODE_WORLD_WRITEABLE);
+      state.edit().clear().commit();
+
+      // User wants to create a new rule, move them to the ActivityChooseRootEvent
+      // activity so they can choose the root event.
+      Intent intent = new Intent();
+      intent.setClass(getApplicationContext(), ActivityChooseRootEvent.class);
+      startActivity(intent);
+      */
+    }
+  };
+
+  /**
+   * View saved rules.
+   */
+  private OnClickListener listenerBtnClickViewRules = new OnClickListener() {
+    public void onClick(View v) {
+      // TODO: Implement showing saved rules activity.
+      UtilUI.showAlert(v.getContext(), "Sorry!", "Viewing saved rules is not yet implemented!");
+    }
+  };
+
+  /**
+   * View logs.
+   */
+  private OnClickListener listenerBtnClickViewLogs = new OnClickListener() {
+    public void onClick(View v) {
+      // TODO: Implement showing logs activity.
+      UtilUI.showAlert(v.getContext(), "Sorry!", "Viewing event logs is not yet implemented!");
+    }
+  };
+
+  /**
+   * Help info.
+   */
+  private OnClickListener listenerBtnClickHelp = new OnClickListener() {
+    public void onClick(View v) {
+      // TODO: Implement showing help activity.
+      UtilUI.showAlert(v.getContext(), "Sorry!", "Help is not yet available!");
+    }
+  };
+}
