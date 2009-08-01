d80f0cd48bc28183a122d93ebba0f8f27b00b47f
dvo203 <dvo203@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-08-01 21:45:29 +0000

    Added support location action parameters.

--- omnidroid/src/edu/nyu/cs/omnidroid/ui/simple/factoryui/FactoryActions.java
+++ omnidroid/src/edu/nyu/cs/omnidroid/ui/simple/factoryui/FactoryActions.java
@@ -26,6 +26,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import edu.nyu.cs.omnidroid.core.datatypes.DataType;
+import edu.nyu.cs.omnidroid.core.datatypes.OmniArea;
 import edu.nyu.cs.omnidroid.core.datatypes.OmniPhoneNumber;
 import edu.nyu.cs.omnidroid.core.datatypes.OmniText;
 import edu.nyu.cs.omnidroid.model.DataTypeIDLookup;
@@ -41,41 +42,43 @@ public class FactoryActions {
 
   private static BuilderOmniPhoneNumber vbOmniPhoneNumber;
   private static BuilderOmniText vbOmniText;
-  
+  private static BuilderOmniArea vbOmniArea;
+
   static {
     // Initialize each of our omni data type builders.
     DataTypeIDLookup lookup = UIDbHelperStore.instance().getDatatypeLookup();
-    vbOmniPhoneNumber = new BuilderOmniPhoneNumber(lookup.getDataTypeID(
-      BuilderOmniPhoneNumber.NAME));
-    vbOmniText = new BuilderOmniText(lookup.getDataTypeID(
-      BuilderOmniText.NAME));
+    vbOmniPhoneNumber = new BuilderOmniPhoneNumber(lookup
+        .getDataTypeID(BuilderOmniPhoneNumber.NAME));
+    vbOmniText = new BuilderOmniText(lookup.getDataTypeID(BuilderOmniText.NAME));
+    vbOmniArea = new BuilderOmniArea(lookup.getDataTypeID(BuilderOmniArea.NAME));
     // TODO: (markww) Add builders for rest of omni data types.
   }
-  
+
   private FactoryActions() {
   }
-  
+
   /**
    * Given an action, build a UI for it based on its parameters and their datatypes.
+   * 
    * @return The fully constructed UI which should be appended to the caller parent.
    */
   public static LinearLayout buildUIFromAction(ModelAction modelAction,
-    ArrayList<DataType> datasOld, Context context) {
-    
+      ArrayList<DataType> datasOld, Context context) {
+
     if (datasOld != null && datasOld.size() != modelAction.getParameters().size()) {
       throw new IllegalStateException(
-        "Old action parameter data array does not much parameter size!");
+          "Old action parameter data array does not much parameter size!");
     }
-    
+
     LinearLayout ll = new LinearLayout(context);
     ll.setLayoutParams(new AbsListView.LayoutParams(LayoutParams.FILL_PARENT,
         LayoutParams.FILL_PARENT));
     ll.setOrientation(LinearLayout.VERTICAL);
-    
+
     int numParameters = modelAction.getParameters().size();
     for (int i = 0; i < numParameters; i++) {
       ModelParameter parameter = modelAction.getParameters().get(i);
-      
+
       // Always add a TextView showing the parameter name.
       addParameterName(parameter, ll);
 
@@ -83,69 +86,71 @@ public class FactoryActions {
       ViewBuilder viewBuilder;
       if (parameter.getDatatype() == vbOmniPhoneNumber.getDatatypeId()) {
         viewBuilder = vbOmniPhoneNumber;
-      }
-      else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
+      } else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
         viewBuilder = vbOmniText;
-      }
-      else {
+      } else if (parameter.getDatatype() == vbOmniArea.getDatatypeId()) {
+        viewBuilder = vbOmniArea;
+      } else {
         throw new IllegalArgumentException("Unsupported datatype encountered!");
       }
-      
+
       // Now append the UI elements required for the type.
       viewBuilder.buildUI(ll, datasOld != null ? datasOld.get(i) : null);
     }
-    
+
     return ll;
   }
-  
+
   /**
-   * Given a layout we previously constructed and an action, extract all user supplied data
-   * from it and construct a ModelRuleAction.
+   * Given a layout we previously constructed and an action, extract all user supplied data from it
+   * and construct a ModelRuleAction.
+   * 
    * @return A ModelRuleAction constructed from the user-supplied data.
-   * @throws Exception  If the user-supplied data is invalid or the supplied layout was not
-   *  created by buildUIFromAction().
+   * @throws Exception
+   *           If the user-supplied data is invalid or the supplied layout was not created by
+   *           buildUIFromAction().
    */
-  public static ModelRuleAction buildActionFromUI(ModelAction modelAction, ViewGroup layout) 
-    throws Exception {
+  public static ModelRuleAction buildActionFromUI(ModelAction modelAction, ViewGroup layout)
+      throws Exception {
 
     ArrayList<DataType> datas = new ArrayList<DataType>();
-    
+
     // Extract all user-supplied information from the UI.
     int childViewPosition = 0;
     int numParameters = modelAction.getParameters().size();
     for (int i = 0; i < numParameters; i++) {
       // Skip over the parameter-name textview.
       childViewPosition++;
-      
+
       ViewBuilder viewBuilder;
       ModelParameter parameter = modelAction.getParameters().get(i);
       if (parameter.getDatatype() == vbOmniPhoneNumber.getDatatypeId()) {
         viewBuilder = vbOmniPhoneNumber;
-      }
-      else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
+      } else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
         viewBuilder = vbOmniText;
-      }
-      else {
+      } else if (parameter.getDatatype() == vbOmniArea.getDatatypeId()) {
+        viewBuilder = vbOmniArea;
+      } else {
         throw new IllegalArgumentException("Unsupported datatype encountered!");
       }
 
       // Add the user-supplied data finally.
       datas.add(viewBuilder.getDataFromView(childViewPosition, layout));
-      
+
       // Advance past the number of views required for this datatype.
       childViewPosition += viewBuilder.getNumControls();
     }
-    
+
     return new ModelRuleAction(-1, modelAction, datas);
   }
-  
+
   /**
-   * Given a layout created by buildUIFromAction(), store all of its current data inside
-   * the supplied prefsEditor.
+   * Given a layout created by buildUIFromAction(), store all of its current data inside the
+   * supplied prefsEditor.
    */
   public static void uiStateSave(ModelAction modelAction, ViewGroup layout,
       SharedPreferences.Editor prefsEditor) {
-    
+
     int childViewPosition = 0;
     int numParameters = modelAction.getParameters().size();
     for (int i = 0; i < numParameters; i++) {
@@ -156,17 +161,17 @@ public class FactoryActions {
       ModelParameter parameter = modelAction.getParameters().get(i);
       if (parameter.getDatatype() == vbOmniPhoneNumber.getDatatypeId()) {
         viewBuilder = vbOmniPhoneNumber;
-      }
-      else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
+      } else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
         viewBuilder = vbOmniText;
-      }
-      else {
+      } else if (parameter.getDatatype() == vbOmniArea.getDatatypeId()) {
+        viewBuilder = vbOmniArea;
+      } else {
         throw new IllegalArgumentException("Unsupported datatype encountered!");
       }
 
       // Finally save the state of this parameter.
       viewBuilder.saveState(prefsEditor, childViewPosition, layout);
-      
+
       // Advance past the number of views required for this datatype.
       childViewPosition += viewBuilder.getNumControls();
     }
@@ -176,8 +181,7 @@ public class FactoryActions {
    * Given a layout created by buildUIFromAction() and data stored by uiStateSave(), load all UI
    * data back into UI elements.
    */
-  public static void uiStateLoad(ModelAction modelAction, ViewGroup layout, 
-      SharedPreferences state) {
+  public static void uiStateLoad(ModelAction modelAction, ViewGroup layout, SharedPreferences state) {
 
     int childViewPosition = 0;
     int numParameters = modelAction.getParameters().size();
@@ -189,24 +193,25 @@ public class FactoryActions {
       ModelParameter parameter = modelAction.getParameters().get(i);
       if (parameter.getDatatype() == vbOmniPhoneNumber.getDatatypeId()) {
         viewBuilder = vbOmniPhoneNumber;
-      }
-      else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
+      } else if (parameter.getDatatype() == vbOmniText.getDatatypeId()) {
         viewBuilder = vbOmniText;
-      }
-      else {
+      } else if (parameter.getDatatype() == vbOmniArea.getDatatypeId()) {
+        viewBuilder = vbOmniArea;
+      } else {
         throw new IllegalArgumentException("Unsupported datatype encountered!");
       }
 
       // Finally restore this control.
       viewBuilder.loadState(state, childViewPosition, layout);
-      
+
       // Advance past the number of views required for this datatype.
       childViewPosition += viewBuilder.getNumControls();
     }
   }
-  
+
   /**
    * Appends a TextView with the name of the supplied parameter to the layout.
+   * 
    * @param parameter
    * @param ll
    */
@@ -215,14 +220,15 @@ public class FactoryActions {
     tv.setText(parameter.getTypeName() + ":");
     layout.addView(tv);
   }
-  
+
   /**
-   * Interface for UI data type builders. Each data type is responsible for being able to generate
-   * a UI for itself, generate a ModelRuleAction, and save and load the UI state.
+   * Interface for UI data type builders. Each data type is responsible for being able to generate a
+   * UI for itself, generate a ModelRuleAction, and save and load the UI state.
    */
   interface ViewBuilder {
     public long getDatatypeId();
 
+    /** Returns number of controls being built */
     public int getNumControls();
 
     public void buildUI(ViewGroup view, DataType dataOld);
@@ -234,7 +240,7 @@ public class FactoryActions {
 
     public void loadState(SharedPreferences state, int childViewPosition, ViewGroup view);
   }
-  
+
   /**
    * Builder for OmniPhoneNumber.
    */
@@ -323,6 +329,81 @@ public class FactoryActions {
       if (state.contains(childViewPosition + "")) {
         EditText et = (EditText) view.getChildAt(childViewPosition);
         et.setText(state.getString(childViewPosition + "", ""));
+      }
+    }
+  }
+
+  /**
+   * Builder for OmniArea.
+   */
+  private static class BuilderOmniArea implements ViewBuilder {
+    public static final String NAME = "Area";
+    private long datatypeId;
+
+    public BuilderOmniArea(long datatypeId) {
+      this.datatypeId = datatypeId;
+    }
+
+    public int getNumControls() {
+      return 4;
+    }
+
+    public long getDatatypeId() {
+      return datatypeId;
+    }
+
+    public void buildUI(ViewGroup view, DataType dataOld) {
+      EditText etAddress = new EditText(view.getContext());
+      EditText etDistance = new EditText(view.getContext());
+      TextView tvAddress = new TextView(view.getContext());
+      TextView tvDistance = new TextView(view.getContext());
+
+      if (dataOld != null) {
+        OmniArea area = (OmniArea) dataOld;
+        etAddress.setText(area.getUserInput());
+        etDistance.setText(Double.toString(area.getProximityDistance()));
+      }
+
+      tvAddress.setText("Address:");
+      tvAddress.setText("Distance (in miles):");
+
+      view.addView(tvAddress);
+      view.addView(etAddress);
+      view.addView(tvDistance);
+      view.addView(etDistance);
+    }
+
+    public DataType getDataFromView(int childViewPosition, ViewGroup view) throws Exception {
+      childViewPosition++; // Skip TextView
+      EditText etAddress = (EditText) view.getChildAt(childViewPosition++);
+      childViewPosition++; // Skip TextView
+      EditText etDistance = (EditText) view.getChildAt(childViewPosition);
+      double distance = Double.parseDouble(etDistance.getText().toString());
+      return new OmniArea(OmniArea.getOmniArea(view.getContext(), etAddress.getText().toString(),
+          distance));
+    }
+
+    public void saveState(SharedPreferences.Editor prefsEditor, int childViewPosition,
+        ViewGroup view) {
+      childViewPosition++; // Skip TextView
+      EditText etAddress = (EditText) view.getChildAt(childViewPosition++);
+      prefsEditor.putString("Address", etAddress.getText().toString());
+      childViewPosition++; // Skip TextView
+      EditText etDistance = (EditText) view.getChildAt(childViewPosition);
+      prefsEditor.putString("Distance", etDistance.getText().toString());
+    }
+
+    public void loadState(SharedPreferences state, int childViewPosition, ViewGroup view) {
+      childViewPosition++;
+      if (state.contains("Address")) {
+        EditText et = (EditText) view.getChildAt(childViewPosition);
+        et.setText(state.getString("Address", ""));
+      }
+      childViewPosition++;
+      childViewPosition++;
+      if (state.contains("Distance")) {
+        EditText et = (EditText) view.getChildAt(childViewPosition);
+        et.setText(state.getString("Distance", ""));
       }
     }
   }
