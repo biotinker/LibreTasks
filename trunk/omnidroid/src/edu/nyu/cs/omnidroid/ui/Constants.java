05505df7fe7a2ca1d4dad3d45cd231c86c3909ca
markww <markww@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-08-02 18:14:19 +0000

    Removed unused UI and test classes after switching to new simple UI.

--- trunk/omnidroid/src/edu/nyu/cs/omnidroid/ui/Constants.java
+++ trunk/omnidroid/src/edu/nyu/cs/omnidroid/ui/Constants.java
@@ -16,29 +16,9 @@
 package edu.nyu.cs.omnidroid.ui;
 
 /**
- * This class simply stores some constants that are used between all the UI classes
- * TODO: (markww) Remove unused constants after done switching to new UI.
+ * Stores codes for use between different UI elements.
  */
 public interface Constants {
-  // Activity Request Codes
-  final int RESULT_ADD_OMNIHANDLER = 1;
-  final int RESULT_DEL_OMNIHANDLER = 2;
-  final int RESULT_EDIT_OMNIHANDLER = 3;
-  final int RESULT_ADD_FILTER = 4;
-  final int RESULT_DEL_FILTER = 5;
-  final int RESULT_EDIT_FILTER = 6;
-  final int RESULT_ADD_THROWER = 7;
-  final int RESULT_DEL_THROWER = 8;
-  final int RESULT_EDIT_THROWER = 9;
-  final int RESULT_ADD_DATUM = 10;
-  final int RESULT_DEL_DATUM = 11;
-  final int RESULT_EDIT_DATUM = 12;
-  final int RESULT_EDIT_SETTINGS = 13;
-
-  // Request Result Values
-  final int RESULT_SUCCESS = 1;
-  final int RESULT_FAILURE = 2;
-  
 
   /** Return code for filter building activities. */
   public static final int ACTIVITY_RESULT_ADD_FILTER = 0;
