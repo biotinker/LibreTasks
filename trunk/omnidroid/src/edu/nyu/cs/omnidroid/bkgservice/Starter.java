9e44f5ea2cdc52d6d3ed0ad8d8fb26ad1abcacc8
EHotou <EHotou@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2009-08-03 04:56:44 +0000

    Code Cleaning: remove not used classes.

--- trunk/omnidroid/src/edu/nyu/cs/omnidroid/bkgservice/Starter.java
+++ trunk/omnidroid/src/edu/nyu/cs/omnidroid/bkgservice/Starter.java
@@ -16,38 +16,26 @@
 package edu.nyu.cs.omnidroid.bkgservice;
 
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
-import edu.nyu.cs.omnidroid.util.OmLogger;
 
-/*Broadcast Receiver to detect system bootup*/
+/**
+ * This broadcast receiver detect intents including System Boot, OmniStart and OmniRestart to
+ * complete necessary operations
+ * */
 public class Starter extends BroadcastReceiver {
-  private ComponentName comp = null;
-  private ComponentName service = null;
 
   public void onReceive(Context context, Intent intent) {
-    // If we're booting up, start our service
-    if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())
-        || "OmniStart".equals(intent.getAction())) {
-      comp = new ComponentName(context.getPackageName(), BRService.class.getName());
-      service = context.startService(new Intent().setComponent(comp));
-      if (null == service) {
-        Toast.makeText(context, "Failed to start OmniDroid Service", 5).show();
-        Log.i("Starter", "Could not start service " + comp.toString());
-        OmLogger.write(context, "Starter could not start Service");
-      }
+    
+    if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+      // Do something when system start
+      
+    } else if ("OmniStart".equals(intent.getAction())) {
+      // Do something when receiving 'OmniStart'
+
     } else if ("OmniRestart".equals(intent.getAction())) {
-      comp = new ComponentName(context.getPackageName(), BRService.class.getName());
-      context.stopService(new Intent().setComponent(comp));
-      service = context.startService(new Intent().setComponent(comp));
-      if (null == service) {
-        Toast.makeText(context, "Failed to start OmniDroid Service", 5).show();
-        Log.i("Starter", "Could not start service " + comp.toString());
-        OmLogger.write(context, "Starter could not start Service");
-      }
+      // Do something when receiving 'OmniRestart'
+      
     }
   }
 }
