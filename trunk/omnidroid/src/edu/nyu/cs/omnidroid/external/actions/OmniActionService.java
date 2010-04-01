b984fbae826f648baeb063b899ae4cd5e0c2043f
beanxlj <beanxlj@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2010-04-01 21:10:32 +0000

    fixed resetDb, refactor OmniActionService

--- trunk/omnidroid/src/edu/nyu/cs/omnidroid/external/actions/OmniActionService.java
+++ trunk/omnidroid/src/edu/nyu/cs/omnidroid/external/actions/OmniActionService.java
@@ -15,52 +15,86 @@
  *******************************************************************************/
 package edu.nyu.cs.omnidroid.external.actions;
 
-import edu.nyu.cs.omnidroid.R;
-import edu.nyu.cs.omnidroid.core.ShowAlertAction;
-import edu.nyu.cs.omnidroid.core.ShowNotificationAction;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+import edu.nyu.cs.omnidroid.R;
+import edu.nyu.cs.omnidroid.core.ShowAlertAction;
+import edu.nyu.cs.omnidroid.core.ShowNotificationAction;
 
 /**
  * This service is for Omnidroid to launch simple actions like: show a message, put some message on
- * notification bar, show a web page etc.
+ * notification bar,etc. More specifically providing execution for actions that is inappropriate 
+ * for an activity to be created.
  */
 public class OmniActionService extends Service {
   
   //operation supported by this service
   public static final String OPERATION_TYPE = "OPERATION_TYPE";
-  public static final String SHOW_ALERT_ACTION = "SHOW_ALERT_ACTION";
-  public static final String SHOW_NOTIFICATION_ACTION = "SHOW_NOTIFICATION_ACTION";
+  public static final int NO_ACTION = -1;
+  public static final int SHOW_ALERT_ACTION = 1;
+  public static final int SHOW_NOTIFICATION_ACTION = 2;
+  public static final int TURN_OFF_WIFI_ACTION = 3;
+  public static final int TURN_ON_WIFI_ACTION = 4;
   
+  //TODO(Roger): chose a more meaningful id
   public static final int NOTIFICATION_ID = 1;
   
 
   @Override
   public IBinder onBind(Intent intent) {
-    // TODO Auto-generated method stub
     return null;
   }
   
   @Override
   public void onStart(Intent intent, int startId) {
     super.onStart(intent, startId);
-    String operationType = intent.getStringExtra(OPERATION_TYPE);
-    if (operationType.equals(SHOW_ALERT_ACTION)) {
+    int operationType = intent.getIntExtra(OPERATION_TYPE, NO_ACTION);
+    switch (operationType) {
+    case SHOW_ALERT_ACTION :
       showAlert(intent);
-    } else if (operationType.equals(SHOW_NOTIFICATION_ACTION)) {
+      break;
+    case SHOW_NOTIFICATION_ACTION :
       showNotification(intent);
-    } else {
+      break;
+    case TURN_OFF_WIFI_ACTION :
+      turnOffWifi();
+      break;
+    case TURN_ON_WIFI_ACTION :
+      turnOnWifi();
+      break;
+    default:
       Log.e("OmniActionSercive", "No such operation supported as: " + operationType);
     }
   }
 
+  /**
+   * turn off the wifi.
+   */
+  private void turnOffWifi() {
+    WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    wifiManager.setWifiEnabled(false);
+  }
+  
+  /**
+   * turn on the wifi.
+   */
+  private void turnOnWifi() {
+    WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+    wifiManager.setWifiEnabled(true);
+  }
+
+  /**
+   * Show a notification on the notification bar.
+   * @param intent provide the message to show
+   */
   private void showNotification(Intent intent) {
     String message = intent.getStringExtra(ShowNotificationAction.PARAM_ALERT_MESSAGE);
     if (message == null) {
@@ -79,6 +113,10 @@ public class OmniActionService extends Service {
     nm.notify(NOTIFICATION_ID, notification);
   }
 
+  /**
+   * Shows a alert window to uer.
+   * @param intent provide the message to show
+   */
   private void showAlert(Intent intent) {
     String message = intent.getStringExtra(ShowAlertAction.PARAM_ALERT_MESSAGE);
     if (message == null) {
