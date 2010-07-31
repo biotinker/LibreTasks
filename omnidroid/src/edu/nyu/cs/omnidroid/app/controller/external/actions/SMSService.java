061fb783daf34366a7b4ebe2822b8a35e2895782
renctan <renctan@ec9cc6c6-4bfe-11de-8f29-b90405f7cc0d>
2010-07-31 21:46:39 +0000

    Fix for issue 147

--- omnidroid/src/edu/nyu/cs/omnidroid/app/controller/external/actions/SMSService.java
+++ omnidroid/src/edu/nyu/cs/omnidroid/app/controller/external/actions/SMSService.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2009 Omnidroid - http://code.google.com/p/omnidroid 
+ * Copyright 2009, 2010 Omnidroid - http://code.google.com/p/omnidroid 
  *  
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
@@ -14,6 +14,8 @@
  * limitations under the License. 
  *******************************************************************************/
 package edu.nyu.cs.omnidroid.app.controller.external.actions;
+
+import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.PendingIntent;
@@ -39,23 +41,14 @@ import edu.nyu.cs.omnidroid.app.controller.actions.SendSmsAction;
  * 
  */
 public class SMSService extends Service {
-
-  private PendingIntent sentPI;
-  private PendingIntent deliveredPI;
-  
-  BroadcastReceiver smsResultReceiver;
+  private BroadcastReceiver smsResultReceiver;
 
   /**
    * attributes field names
    */
-  public static final String SENT = "SMS_SENT";
-  public static final String DELIVERED = "SMS_DELIVERED";
+  public static final String INTENT_ACTION_SENT = "SMS_SENT";
+  public static final String INTENT_ACTION_DELIVERED = "SMS_DELIVERED";
 
-
-  private String phoneNumber;
-  private String textMessage;
-
-  private Intent actionIntent;
   /**
    * @return null because client can't bind to this service
    */
@@ -76,54 +69,82 @@ public class SMSService extends Service {
    *          <li>startId : A unique integer representing this specific request to start
    *          </ol>
    */
-  public void onStart(Intent intent, int startId) {
+  public void onStart(final Intent intent, int startId) {
     super.onStart(intent, startId);
-    actionIntent = intent;
-
-    sentPI = PendingIntent.getBroadcast(this, 0, new Intent(SENT), 0);
-    deliveredPI = PendingIntent.getBroadcast(this, 0, new Intent(DELIVERED), 0);
-
-    phoneNumber = intent.getStringExtra(SendSmsAction.PARAM_PHONE_NO);
-    textMessage = intent.getStringExtra(SendSmsAction.PARAM_SMS);
 
     Toast.makeText(this, "SMS Service Started", Toast.LENGTH_LONG).show();
 
-    smsResultReceiver = new  BroadcastReceiver() {
+    smsResultReceiver = new BroadcastReceiver() {
       @Override
       public void onReceive(Context context, Intent intent) {
         switch (getResultCode()) {
         case Activity.RESULT_OK:
-          ResultProcessor.process(context, actionIntent, ResultProcessor.RESULT_SUCCESS,
+          ResultProcessor.process(context, intent, ResultProcessor.RESULT_SUCCESS,
               getString(R.string.sms_sent));
           break;
         case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-          ResultProcessor.process(context, actionIntent, ResultProcessor.RESULT_FAILURE_UNKNOWN, 
+          ResultProcessor.process(context, intent, ResultProcessor.RESULT_FAILURE_UNKNOWN,
               getString(R.string.sms_failed_generic_failure));
           break;
         case SmsManager.RESULT_ERROR_NO_SERVICE:
-          ResultProcessor.process(context, actionIntent, ResultProcessor.RESULT_FAILURE_SERVICE,
+          ResultProcessor.process(context, intent, ResultProcessor.RESULT_FAILURE_SERVICE,
               getString(R.string.sms_failed_no_service));
           break;
         case SmsManager.RESULT_ERROR_NULL_PDU:
-          ResultProcessor.process(context, actionIntent, ResultProcessor.RESULT_FAILURE_UNKNOWN, 
+          ResultProcessor.process(context, intent, ResultProcessor.RESULT_FAILURE_UNKNOWN,
               getString(R.string.sms_failed_null_pdu));
           break;
         case SmsManager.RESULT_ERROR_RADIO_OFF:
-          ResultProcessor.process(context, actionIntent, ResultProcessor.RESULT_FAILURE_SERVICE, 
+          ResultProcessor.process(context, intent, ResultProcessor.RESULT_FAILURE_SERVICE,
               getString(R.string.sms_failed_radio_off));
           break;
         }
       }
     };
-    registerReceiver(smsResultReceiver, new IntentFilter(SENT));
+    registerReceiver(smsResultReceiver, new IntentFilter(INTENT_ACTION_SENT));
+
+    final String phoneNumber = intent.getStringExtra(SendSmsAction.PARAM_PHONE_NO);
+    final String textMessage = intent.getStringExtra(SendSmsAction.PARAM_SMS);
+    final SMSService smsServiceInstance = this;
 
     TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
     tm.listen(new PhoneStateListener() {
       public void onCallStateChanged(int state, String incomingNumber) {
         // send SMS when phone is not ringing.
         if (state != TelephonyManager.CALL_STATE_RINGING) {
-          SmsManager sms = SmsManager.getDefault();
-          sms.sendTextMessage(phoneNumber, null, textMessage, sentPI, deliveredPI);
+          final SmsManager sms = SmsManager.getDefault();
+
+          /*
+           * Split SMS message into chunks to avoid exceeding 160 characters, which will cause null
+           * pointer exception (not documented in official API reference, but a lot of other people
+           * experienced the same problem).
+           */
+          ArrayList<String> splitTextMessages = sms.divideMessage(textMessage);
+
+          for (final String message : splitTextMessages) {
+            Intent sentIntent = new Intent(intent);
+            intent.setAction(INTENT_ACTION_SENT);
+            intent.removeExtra(SendSmsAction.PARAM_SMS);
+            intent.putExtra(SendSmsAction.PARAM_SMS, message);
+
+            final PendingIntent sentPI = PendingIntent.getBroadcast(smsServiceInstance, 0,
+                sentIntent, 0);
+            
+            Thread smsThread = new Thread(new Runnable() {
+              public void run() {
+                /*
+                 * TODO (renctan): Consider using sendMultipartTextMessage() in the future. The
+                 * reason why sendTextMessage() was used instead is because it appears that there is
+                 * a bug in API 4 that will convert the SMS message into garbage characters in the
+                 * receiving end.
+                 */
+                sms.sendTextMessage(phoneNumber, null, message, sentPI, null);
+              }
+            });
+
+            smsThread.start();
+          }
+          
           // Update the Android SMS application
           ContentValues values = new ContentValues();
           values.put("address", phoneNumber);
@@ -137,7 +158,7 @@ public class SMSService extends Service {
       }
     }, PhoneStateListener.LISTEN_CALL_STATE);
   }
-  
+
   @Override
   public void onDestroy() {
     super.onDestroy();
